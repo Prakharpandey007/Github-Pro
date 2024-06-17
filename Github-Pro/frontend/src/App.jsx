d3f276b8391c4532d1346e0a903102db744f56ca
prakhar <prakharharshit.2003@gmail.com>
2024-06-17 22:22:13 +0530

    setup-basis project setup

---
+++ Github-Pro/frontend/src/App.jsx
@@ -0,0 +1,30 @@
+import { Route, Routes } from "react-router-dom";
+import LoginPage from "./pages/LoginPage";
+import HomePage from "./pages/HomePage";
+import SignupPage from "./pages/SignupPage";
+import LikesPage from "./pages/LikesPage";
+import Explore from "./pages/Explore";
+import Sidebar from "./components/Sidebar";
+function App() {
+  return (
+    //sidebar and footer arre outside routes so it is present in every page
+    <>
+      <div className="flex text-white">
+       
+        <Sidebar />
+        <div className="max-w-5xl my-5 text-white mx-auto transition-all duration-300 flex-1">
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/signup" element={<SignupPage />} />
+            <Route path="/explore" element={<Explore/>} />
+            <Route path="/likes" element={<LikesPage />} />
+          </Routes>
+          <footer>footer</footer>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
