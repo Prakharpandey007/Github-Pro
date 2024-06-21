092f9ce1288dcbebfe0524d1fd10e786e61f8710
prakhar <prakharharshit.2003@gmail.com>
2024-06-21 18:24:03 +0530

    add-explore repo

--- Github-Pro/frontend/src/components/SortRepos.jsx
+++ Github-Pro/frontend/src/components/SortRepos.jsx
@@ -1,21 +1,26 @@
-const SortRepos = () => {
+const SortRepos = ({onSort,sortType}) => {
+	
+
 	return (
 		<div className='mb-2 flex justify-center lg:justify-end'>
 			<button
 				type='button'
-				className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+				className={`py-2.5 px-5 me-2 mb-2 text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass ${sortType==="recent"?"border-blue-800":""}`}
+				onClick={()=>onSort("recent")}
 			>
 				Most Recent
 			</button>
 			<button
 				type='button'
-				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass ${sortType==="stars"?"border-blue-800":""}`}
+				onClick={()=>onSort("stars")}
 			>
 				Most Stars
 			</button>
 			<button
 				type='button'
-				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass`}
+				className={`py-2.5 px-5 me-2 mb-2  text-xs sm:text-sm font-medium focus:outline-none rounded-lg bg-glass ${sortType==="folks"?"border-blue-800":""}`}
+				onClick={()=>onSort("folks")}
 			>
 				Most Forks
 			</button>
--- Github-Pro/frontend/src/pages/HomePage.jsx
+++ Github-Pro/frontend/src/pages/HomePage.jsx
@@ -1,68 +1,83 @@
-import Search from '../components/Search'
-import SortRepos from '../components/SortRepos'
-import ProfileInfo from '../components/ProfileInfo'
-import Repos from '../components/Repos'
-import { useState } from 'react'
-import { useEffect,useCallback } from 'react'
-import toast from 'react-hot-toast'
-import Spinner from '../components/Spinner'
-const HomePage=()=>{
-  const[userProfile,setUserProfile]=useState(null);
-  const[repos,setRepos]=useState([]);
-  const[loading,setLoading]=useState(false);
+import Search from "../components/Search";
+import SortRepos from "../components/SortRepos";
+import ProfileInfo from "../components/ProfileInfo";
+import Repos from "../components/Repos";
+import { useState } from "react";
+import { useEffect, useCallback } from "react";
+import toast from "react-hot-toast";
+import Spinner from "../components/Spinner";
+const HomePage = () => {
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-  const[sortType,setSortType]=useState('forks');
+  const [sortType, setSortType] = useState("recent");
 
-  const getUserProfileAndRepos=useCallback(async(username="Prakharpandey007")=>{
-    setLoading(true);
-    try {
-      const userResponse=await fetch(`https://api.github.com/users/${username}`);
-      const userProfile=await userResponse.json();
-      setUserProfile(userProfile);
+  const getUserProfileAndRepos = useCallback(
+    async (username = "Prakharpandey007") => {
+      setLoading(true);
+      try {
+        const userResponse = await fetch(
+          `https://api.github.com/users/${username}`,
+          {}
+        );
+        const userProfile = await userResponse.json();
+        setUserProfile(userProfile);
 
-      const repoResponse=await fetch(userProfile.repos_url);
-      const repos=await repoResponse.json();
-      setRepos(repos);
-      // console.log("userProfile",userProfile);
-      // console.log("repos:",repos);
-      return{userProfile,repos};
-        } 
-        catch (error) {
-      toast.error(error.message)
-    }
-    finally{
-      setLoading(false);
-    }
-  },[])
+        const repoResponse = await fetch(userProfile.repos_url);
+        const repos = await repoResponse.json();
+        setRepos(repos);
+        // console.log("userProfile",userProfile);
+        // console.log("repos:",repos);
+        return { userProfile, repos };
+      } catch (error) {
+        toast.error(error.message);
+      } finally {
+        setLoading(false);
+      }
+    },
+    []
+  );
 
-  useEffect(()=>{
+  useEffect(() => {
     getUserProfileAndRepos();
+  }, [getUserProfileAndRepos]);
 
-  },[getUserProfileAndRepos])
+  const onSearch = async (e, username) => {
+    e.preventDefault();
+    setLoading(true);
+    setRepos([]);
+    setUserProfile(null);
+    const { userProfile, repos } = await getUserProfileAndRepos(username);
+    setUserProfile(userProfile);
+    setRepos(repos);
+    setLoading(false);
+    setSortType("recent");
+  };
+  const onSort = (sortType) => {
+    if (sortType === "recent") {
+      repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, recent first
+    } else if (sortType === "stars") {
+      repos.sort((a, b) => b.stargazers_count - a.stargazers_count); //descending, most stars first
+    } else if (sortType === "forks") {
+      repos.sort((a, b) => b.forks_count - a.forks_count); //descending, most forks first
+    }
+    setSortType(sortType);
+    setRepos([...repos]);
+  };
 
-const onSearch=async(e,username)=>{
-e.preventDefault();
-setLoading(true);
-setRepos([]);
-setUserProfile(null);
-const {userProfile,repos}=await getUserProfileAndRepos(username);
-setUserProfile(userProfile);
-setRepos(repos);
-setLoading(false);
-
-}
-  return(
+  return (
     <div className="m-4">
-      <Search onSearch={onSearch}/>
-      <SortRepos/>
+      <Search onSearch={onSearch} />
+      {repos.length > 0 && <SortRepos onSort={onSort} SortType={sortType} />}
+
       <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
-       { userProfile && !loading && <ProfileInfo userProfile={userProfile}/>}
-       {repos.length>0 && !loading && <Repos repos={repos}/>}
-        {loading && <Spinner/>}
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
 
+        {!loading && <Repos repos={repos} />}
+        {loading && <Spinner />}
       </div>
-
     </div>
-  )
-}
-export default HomePage
+  );
+};
+export default HomePage;
