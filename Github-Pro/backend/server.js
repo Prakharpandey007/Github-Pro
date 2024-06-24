b1b7c0cf9539e8e3579db2e91d07aeab68fa7e03
prakhar <prakharharshit.2003@gmail.com>
2024-06-24 23:34:48 +0530

    code completed

--- Github-Pro/backend/server.js
+++ Github-Pro/backend/server.js
@@ -8,9 +8,12 @@ import authRoutes from './routes/authroutes.js';
 import './passport/githubauth.js'
 import passport from 'passport';
 import session from 'express-session';
-
+import path from "path";
 dotenv.config();
-
+const PORT=process.env.PORT ||5000
+//run server and client on same doamin
+const __dirname=path.resolve();
+console.log("dirname",__dirname);
 const app=express();
 app.use(session({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
 // Initialize Passport!  Also use passport.session() middleware, to support
@@ -19,13 +22,15 @@ app.use(passport.initialize());
 app.use(passport.session());
 app.use(cors());
 
-app.get("/",(req,res)=>{
-    res.send("server is ready");
-});
+
 app.use("/api/auth",authRoutes);
 app.use("/api/users",userRoutes);
 app.use("/api/explore",exploreRoutes);
-app.listen(8080,()=>{
-    console.log("server started on port 8080");
+app.use(express.static(path.join(__dirname,"/frontend/dist")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
+});
+app.listen(PORT,()=>{
+    console.log(`server started on port  ${PORT}`);
     connectMongoDB();
 })
